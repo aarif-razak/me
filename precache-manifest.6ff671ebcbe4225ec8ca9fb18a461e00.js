ac224cbc35823a18f7cbef47dbf46859f9e491ec
Aarif <51032249+aarif-razak@users.noreply.github.com>
2020-07-07 23:49:33 -0400

    Updates

---
+++ precache-manifest.6ff671ebcbe4225ec8ca9fb18a461e00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4c999cd9a336763837f14b1c265e16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4167fda08a337089eec5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c321db344cdfe4b65eb4",
+    "url": "/static/js/2.beb74294.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.beb74294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4167fda08a337089eec5",
+    "url": "/static/js/main.8279768b.chunk.js"
+  },
+  {
+    "revision": "766df7dbb5392e1314ed",
+    "url": "/static/js/runtime-main.9c9c41ef.js"
+  }
+]);
