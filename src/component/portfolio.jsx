36c69a026ed4dd81db0e21082ddb9bb320e95420
Aarif <51032249+aarif-razak@users.noreply.github.com>
2020-07-18 19:24:47 -0400

    Removed Debugging Section (inconsistent)

--- src/component/portfolio.jsx
+++ src/component/portfolio.jsx
@@ -35,7 +35,7 @@ export default class Portfolio extends Component{
           will send NASA's Photo on that specified day. Also built a simple frontend with instructions.
         </p>
         <ul>
-          <li><b>Debugging: </b>Debugging was done via the Twilio Docs, along with experimentation using NASA's Photo API on various devices. </li>
+          {/* <li><b>Debugging: </b>Debugging was done via the Twilio Docs, along with experimentation using NASA's Photo API on various devices. </li> */}
           <li><b>Built With: </b>Python, Flask, HTML, CSS, and an iPhone 8.</li>
         </ul>
       </div>
@@ -43,7 +43,7 @@ export default class Portfolio extends Component{
       <div className="panel" dir="ltr">
         <p>Built the famous dice game known as the Pig Game into an interactive web application. It features a 'realistic' dice rolling, along with scorekeeping for two players.</p>
         <ul>
-          <li><b>Debugging: </b>Debugging was done via Mozilla's Docs, along with W3School's extensive library of resources.</li>
+          {/* <li><b>Debugging: </b>Debugging was done via Mozilla's Docs, along with W3School's extensive library of resources.</li> */}
           <li><b>Built with: </b> Pure Javascript, HTML, and CSS. Media taken from various sources, such as stock images or Destiny 2 screenshots.</li>
         </ul>
       </div>
@@ -63,7 +63,7 @@ export default class Portfolio extends Component{
           sharpen up my skills. (And tell myself I'm doing something productive.)
         </p>
         <ul>
-          <li><b>Debugging: </b> Debugging was done via React Docs, freeCodeCamp, and W3Schools</li>
+          {/* <li><b>Debugging: </b> Debugging was done via React Docs, freeCodeCamp, and W3Schools</li> */}
           <li><b>Built With: </b>HTML, CSS, React, Javascript, and dry humor. </li>
         </ul>
       </div>
